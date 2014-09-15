0c5feb73b069a20c75d132730f0b09b70ab24306
Adam Eivy <adam.eivy@disney.com>
2014-09-15 15:26:26 -0700

    round 1

---
+++ README.md
@@ -0,0 +1,75 @@
+# \\[._.]/ - Hapi Authorization Header
+
+[![](http://img.shields.io/gratipay/antic.svg?style=flat)](https://gratipay.com/antic)
+[![](http://img.shields.io/npm/dm/hapi-auth-header.svg?style=flat)](https://www.npmjs.org/package/hapi-auth-header)
+[![](http://img.shields.io/npm/v/hapi-auth-header.svg?style=flat)](https://www.npmjs.org/package/hapi-auth-header)
+[![](http://img.shields.io/codeclimate/github/atomantic/hapi-auth-header.svg?style=flat)](https://codeclimate.com/github/atomantic/hapi-auth-header)
+[![](http://img.shields.io/codeclimate/coverage/github/atomantic/hapi-auth-header.svg?style=flat)](https://codeclimate.com/github/atomantic/hapi-auth-header)
+[![](http://img.shields.io/travis/atomantic/hapi-auth-header.svg?style=flat)](https://travis-ci.org/atomantic/hapi-auth-header)
+[![](http://img.shields.io/david/atomantic/hapi-auth-header.svg?style=flat)](https://www.npmjs.org/package/hapi-auth-header)
+
+[**hapi**](https://github.com/spumko/hapi) Authorization header authentication scheme
+
+## Note
+
+Special thanks to @johnbrett for [hapi-auth-bearer-token plugin](https://www.npmjs.org/package/hapi-auth-bearer-token), which this plugin used as scaffolding.
+
+You might find that the [hapi-auth-bearer-token plugin](https://www.npmjs.org/package/hapi-auth-bearer-token) is all you need, which also allows for `Bearer` supplied as a query param (this module does not). This plugin exists to handle the whole Authorization header, which allows semicolon delimited Authorization sources. This is useful if you need to validate more than one Authorization header field. For example, the API that this plugin was originally built for was gated by an API management proxy, which forwards traffic along with an "FD" Authorization header, added along with the "Bearer" header like so: `Authorization: FD AF6C74D1-BBB2-4171-8EE3-7BE9356EB018; Bearer 12345678`
+
+This plugin is identical to [hapi-auth-bearer-token plugin](https://www.npmjs.org/package/hapi-auth-bearer-token) except that it will return `tokens` as an object back to your callback rather than `token` as the `Bearer` field value (e.g. `Authorization: FD AF6C74D1-BBB2-4171-8EE3-7BE9356EB018; Bearer 12345678` would result in your validate function being called with tokens set to
+```
+{
+   Bearer: 12345678,
+   FD: AF6C74D1-BBB2-4171-8EE3-7BE9356EB018
+}
+```
+
+## Use
+Authentication requires validating tokens passed by Authentication header. You can customize any arbitrary number of names for the Authorization header fields that you want to validate.
+
+Bearer authentication requires validating a token passed in by either the bearer authorization header, or by an access_token query parameter. The `'auth-header'` scheme takes the following options:
+
+- `validateFunc` - (required) A validation function with the signature `function(tokens, callback)` where:
+    - `tokens` - an object containing each of the Authorization header keys.
+    - `callback` - a callback function with the signature `function(err, isValid, credentials)` where:
+        - `err` - an internal error.
+        - `isValid` - `true` if both the username was found and the password matched, otherwise `false`.
+        - `credentials` - a credentials object passed back to the application in `request.auth.credentials`. Typically, `credentials` are only
+          included when `isValid` is `true`, but there are cases when the application needs to know who tried to authenticate even when it fails
+          (e.g. with authentication mode `'try'`).
+- `options` - (optional) - there are currently no configuration options :)
+
+```javascript
+var Hapi = require('hapi');
+
+var defaultHandler = function (request, reply) {
+    reply('success');
+};
+
+var server = Hapi.createServer('localhost', 8080, {
+    cors: true
+});
+
+server.pack.register(require('hapi-auth-header'), function (err) {
+
+    server.auth.strategy('header', 'auth-header', {
+        validate: function( tokens, callback ) {
+            // Use a real strategy here,
+            // e.g. send the token to an oauth validation API
+            if(tokens.Bearer === "1234" && tokens.oauth === "4321" ){
+                callback(null, true, { token: tokens.bearer })
+            } else {
+                callback(null, false, { token: tokens.bearer })
+            }
+        }
+    });
+
+    server.route({ method: 'GET', path: '/', handler: defaultHandler, config: { auth: 'header' } });
+
+    server.start(function () {
+        console.log('Server started at: ' + server.info.uri);
+    })
+});
+```
+
+License MIT @ Adam Eivy 2014
---
+++ test/index.js
@@ -0,0 +1,149 @@
+var Lab = require('lab');
+var Hapi = require('hapi');
+var lab = exports.lab = Lab.script();
+
+var expect = Lab.expect;
+var before = lab.before;
+var after = lab.after;
+var describe = lab.describe;
+var it = lab.it;
+
+describe('Authorization', function () {
+
+    var defaultHandler = function (request, reply) {
+        reply('success');
+    };
+
+    var server = new Hapi.Server({debug: false});
+    before(function(done){
+
+        server.pack.register(require('../'), function (err) {
+            expect(err).to.not.exist;
+
+            server.auth.strategy('default', 'auth-header', true, {
+                validateFunc: function(tokens, callback) {
+                   return callback(null, tokens.Bearer==="12345678",  tokens);
+                }
+            });
+
+            server.auth.strategy('multi', 'auth-header', {
+                validateFunc: function(tokens, callback) {
+                   return callback(
+                       null,
+                       tokens.Bearer==="12345678" && tokens.gateway==='87654321',
+                       tokens
+                   );
+                }
+            });
+
+            server.auth.strategy('always_reject', 'auth-header', {
+                validateFunc: function(tokens, callback) {
+                   return callback(null, false, tokens);
+                }
+            });
+
+            server.auth.strategy('with_error_strategy', 'auth-header', {
+                validateFunc: function(tokens, callback) {
+                    return callback({'Error':'Error'}, false, null);
+                }
+            });
+
+            server.auth.strategy('no_credentials', 'auth-header', {
+                validateFunc: function(tokens, callback) {
+                    return callback(null, true, null);
+                }
+            });
+
+            server.route([
+                { method: 'POST', path: '/basic', handler: defaultHandler, config: { auth: 'default' } },
+                { method: 'POST', path: '/basic_default_auth', handler: defaultHandler, config: { } },
+                { method: 'GET', path: '/basic_validate_error', handler: defaultHandler, config: { auth: 'with_error_strategy' } },
+                { method: 'GET', path: '/always_reject', handler: defaultHandler, config: { auth: 'always_reject' } },
+                { method: 'GET', path: '/no_credentials', handler: defaultHandler, config: { auth: 'no_credentials' } },
+                { method: 'GET', path: '/multi', handler: defaultHandler, config: { auth: 'multi' } }
+            ]);
+
+            done();
+        });
+    });
+
+    after(function(done) {
+        server = null;
+        done();
+    });
+
+    it('returns 200 and success with correct bearer token header set', function (done) {
+        var request = { method: 'POST', url: '/basic', headers: { authorization: "Bearer 12345678" } };
+        server.inject(request, function (res) {
+            expect(res.statusCode).to.equal(200);
+            expect(res.result).to.equal('success');
+            done();
+        });
+    });
+
+    it('returns 401 error when no bearer token is set when one is required by default', function (done) {
+        var request = { method: 'POST', url: '/basic_default_auth' };
+        server.inject(request, function (res) {
+            expect(res.statusCode).to.equal(401);
+            done();
+        });
+    });
+
+    it('returns 401 when bearer authorization header is not set', function (done) {
+        var request = { method: 'POST', url: '/basic', headers: { authorization: 'definitelynotacorrecttoken' } };
+        server.inject(request, function (res) {
+            expect(res.statusCode).to.equal(401);
+            done();
+        });
+    });
+
+    it('returns 401 error with object bearer token type (invalid token)', function (done) {
+        var request = { method: 'POST', url: '/basic', headers: { authorization: 'Bearer {test: 1}' } };
+        server.inject(request, function (res) {
+            expect(res.statusCode).to.equal(401);
+            done();
+        });
+    });
+
+    it('returns 200 when using multi and passing multiple good auth tokens', function (done) {
+        var request = { method: 'GET', url: '/multi', headers: { authorization: 'Bearer 12345678; gateway 87654321' } };
+        server.inject(request, function (res) {
+            expect(res.statusCode).to.equal(200);
+            expect(res.result).to.equal('success');
+            done();
+        });
+    });
+
+    it('returns 401 when using multi and one of the tokens is missing', function (done) {
+        var request = { method: 'GET', url: '/multi', headers: { authorization: 'Bearer 12345678' } };
+        server.inject(request, function (res) {
+            expect(res.statusCode).to.equal(401);
+            done();
+        });
+    });
+
+    it('returns 500 when strategy returns an error to validateFunc', function (done) {
+        var request = { method: 'GET', url: '/basic_validate_error', headers: { authorization: 'Bearer 12345678' } };
+        server.inject(request, function (res) {
+            expect(res.statusCode).to.equal(500);
+            expect(JSON.stringify(res.result)).to.equal("{\"Error\":\"Error\"}");
+            done();
+        });
+    });
+
+    it('returns 401 handles when isValid false passed to validateFunc', function (done) {
+        var request = { method: 'GET', url: '/always_reject', headers: { authorization: 'Bearer 12345678' } };
+        server.inject(request, function (res) {
+            expect(res.statusCode).to.equal(401);
+            done();
+        });
+    });
+
+    it('returns 500 when no credentials passed to validateFunc', function (done) {
+        var request = { method: 'GET', url: '/no_credentials', headers: { authorization: 'Bearer 12345678' } };
+        server.inject(request, function (res) {
+            expect(res.statusCode).to.equal(500);
+            done();
+        });
+    });
+});
